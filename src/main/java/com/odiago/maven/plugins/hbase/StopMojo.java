30ec3947fc366e6794e9a9581f2097b3428a2b61
Garrett Wu <wugarrett@gmail.com>
2011-07-29 14:15:55 -0700

    Configured findbugs and checkstyle.  Added 'stop' goal.

---
+++ src/main/java/com/odiago/maven/plugins/hbase/StopMojo.java
@@ -0,0 +1,20 @@
+// (c) Copyright 2011 Odiago, Inc.
+
+package com.odiago.maven.plugins.hbase;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * A maven goal that stops the mini HBase cluster started by the 'start' goal.
+ *
+ * @goal stop
+ * @phase post-integration-test
+ */
+public class StopMojo extends AbstractMojo {
+  /** {@inheritDoc} */
+  @Override
+  public void execute() throws MojoExecutionException {
+    // TODO
+  }
+}
---
+++ src/main/java/com/odiago/maven/plugins/hbase/package-info.java
@@ -0,0 +1,6 @@
+// (c) Copyright 2011 Odiago, Inc.
+
+/**
+ * A maven plugin that allows integration tests to run with a mini-hbase cluster.
+ */
+package com.odiago.maven.plugins.hbase;
