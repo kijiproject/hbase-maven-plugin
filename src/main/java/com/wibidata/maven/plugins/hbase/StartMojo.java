57e4436f8cb72d4cdd1fd32ce40b70b03d08b067
Kiyan Ahmadizadeh <kiyan@wibidata.com>
2012-08-28 16:51:14 -0700

    Plugin can save contents of hbase.maven.plugin to project build directory.

--- src/main/java/com/wibidata/maven/plugins/hbase/StartMojo.java
+++ src/main/java/com/wibidata/maven/plugins/hbase/StartMojo.java
@@ -28,6 +28,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
@@ -67,6 +68,16 @@ public class StartMojo extends AbstractMojo {
    * @parameter property="skip" default-value="false"
    */
   private boolean mSkip;
+
+  /**
+   * If true, the Hadoop temporary directory (given by Hadoop configuration property hadoop.tmp
+   * .dir) will be cleared before the cluster is started, then copied to the project's build
+   * directory before the cluster is shutdown.
+   *
+   * @parameter property="saveHadoopTmpDir" expression="${save.hadoop.tmp}" default-value="false"
+   * @required
+   */
+  private boolean mSaveHadoopTmpDir;
 
   /**
    * The file that will store the configuration required to connect to the started mini HBase
@@ -151,6 +162,18 @@ public class StartMojo extends AbstractMojo {
    */
   public void setSkip(boolean skip) {
     mSkip = skip;
+  }
+
+  /**
+   * Sets whether the Hadoop temporary directory, given by hadoop.tmp.dir, should be cleared
+   * before the cluster is started and copied to the project build directory before the cluster
+   * is shutdown.
+   *
+   * @param saveTempDir If true, the directory will be copied to the project build directory
+   *     before the cluster is shutdown.
+   */
+  public void setSaveHadoopTmpDir(boolean saveTempDir) {
+    mSaveHadoopTmpDir = saveTempDir;
   }
 
   /**
@@ -252,6 +275,11 @@ public class StartMojo extends AbstractMojo {
       }
     }
 
+    // If necessary, clear the Hadoop tmp dir.
+    if (mSaveHadoopTmpDir) {
+      removeHadoopTmpDir(conf);
+    }
+
     // Start the cluster.
     try {
       MiniHBaseClusterSingleton.INSTANCE.startAndWaitUntilReady(
@@ -272,6 +300,31 @@ public class StartMojo extends AbstractMojo {
     // Write the configuration index.
     if (mWriteConfIndex) {
       writeConfigurationIndex();
+    }
+  }
+
+  /**
+   * Deletes the directory given by hadoop.tmp.dir in the specified configuration. The
+   * MapReduce cluster started by this plugin will store logs for job tasks in a job-specific
+   * directory under hadoop.tmp.dir/userlogs. The
+   * {@link org.apache.hadoop.hbase.HBaseTestingUtility} will delete log files on shutdown but
+   * not the directory structure, making it hard to locate specific job logs after multiple runs.
+   * Clearing hadoop.tmp.dir before the cluster starts again alleviates this problem.
+   *
+   * @param conf A Hadoop configuration used to determine the value of hadoop.tmp.dir.
+   */
+  private void removeHadoopTmpDir(Configuration conf) {
+    String hadoopTmpPath = conf.get("hadoop.tmp.dir");
+    File hadoopTmp = new File(hadoopTmpPath);
+    if (hadoopTmp.exists()) {
+      getLog().info("Deleting Hadoop tmp dir " + hadoopTmp.toString() + " because it already " +
+          "exists.");
+      try {
+        FileUtils.deleteDirectory(hadoopTmp);
+        getLog().info("Successfully deleted Hadoop tmp dir: " + hadoopTmp.toString());
+      } catch (IOException e) {
+        getLog().warn("An existing Hadoop tmp dir could not be deleted.", e);
+      }
     }
   }
 
--- src/main/java/com/wibidata/maven/plugins/hbase/StopMojo.java
+++ src/main/java/com/wibidata/maven/plugins/hbase/StopMojo.java
@@ -17,6 +17,10 @@
 
 package com.wibidata.maven.plugins.hbase;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
@@ -27,9 +31,90 @@ import org.apache.maven.plugin.MojoExecutionException;
  * @phase post-integration-test
  */
 public class StopMojo extends AbstractMojo {
+
+  /**
+   * If true, this goal should be a no-op.
+   *
+   * @parameter property="skip" default-value="false"
+   */
+  private boolean mSkip;
+
+  /**
+   * The build directory for this project.
+   *
+   * @parameter property="projectBuildDir" default-value="${project.build.directory}"
+   * @required
+   */
+  private String mProjectBuildDir;
+
+  /**
+   * If true, the Hadoop temporary directory (given by Hadoop configuration property hadoop.tmp
+   * .dir) will be cleared before the cluster is started, then copied to the project's build
+   * directory before the cluster is shutdown.
+   *
+   * @parameter property="saveHadoopTmpDir" expression="${save.hadoop.tmp}" default-value="false"
+   * @required
+   */
+  private boolean mSaveHadoopTmpDir;
+
+  /**
+   * Sets the output directory for the project's build.
+   *
+   * @param dir The path to the output directory.
+   */
+  public void setProjectBuildDir(String dir) {
+    mProjectBuildDir = dir;
+  }
+
+  /**
+   * Sets whether this goal should be a no-op.
+   *
+   * @param skip If true, this goal should do nothing.
+   */
+  public void setSkip(boolean skip) {
+    mSkip = skip;
+  }
+
+  /**
+   * Sets whether the Hadoop temporary directory, given by hadoop.tmp.dir, should be cleared
+   * before the cluster is started and copied to the project build directory before the cluster
+   * is shutdown.
+   *
+   * @param saveTempDir If true, the directory will be copied to the project build directory
+   *     before the cluster is shutdown.
+   */
+  public void setSaveHadoopTmpDir(boolean saveTempDir) {
+    mSaveHadoopTmpDir = saveTempDir;
+  }
+
+  /**
+   * Copies the directory indicated by hadoop.tmp.dir in the mini-cluster's configuration to the
+   * project build directory.
+   */
+  private void copyHadoopTmpDir() {
+    String tmpDirProperty =
+        MiniHBaseClusterSingleton.INSTANCE.getClusterConfiguration().get("hadoop.tmp.dir");
+    File hadoopTmp = new File(tmpDirProperty);
+    File hadoopTmpCopy = new File(new File(mProjectBuildDir), "hadoop-tmp");
+    getLog().info("Copying " + hadoopTmp.toString() + " to " + hadoopTmpCopy.toString());
+    try {
+      FileUtils.copyDirectory(hadoopTmp, hadoopTmpCopy);
+      getLog().info("Successfully copied hadoop tmp dir.");
+    } catch (IOException e) {
+      getLog().warn("The Hadoop tmp dir could not be copied to the project's build directory.", e);
+    }
+  }
+
   /** {@inheritDoc} */
   @Override
   public void execute() throws MojoExecutionException {
+    if (mSkip) {
+      getLog().info("Not stopping an HBase cluster because skip=true.");
+      return;
+    }
+    if (mSaveHadoopTmpDir) {
+      copyHadoopTmpDir();
+    }
     MiniHBaseClusterSingleton.INSTANCE.stop(getLog());
   }
 }
